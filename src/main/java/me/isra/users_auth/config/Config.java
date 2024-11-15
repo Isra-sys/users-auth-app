673bb66b8d9e56e5b1e409492932e5d8a507dd92
Isra <israc008@outlook.es>
2024-11-15 22:59:07 +0100

    Config changed

--- src/main/java/me/isra/users_auth/config/Config.java
+++ src/main/java/me/isra/users_auth/config/Config.java
@@ -3,79 +3,25 @@ package me.isra.users_auth.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
-import me.isra.users_auth.auth.repository.Token;
-import me.isra.users_auth.auth.repository.TokenRepository;
 import me.isra.users_auth.user.User;
 import me.isra.users_auth.user.UserRepository;
 
-@Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity
 public class Config {
-
-    private final JwtAuthFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-    private final TokenRepository tokenRepository;
-    private final CorsConfigurationSource corsConfigurationSource;
-    private final UserRepository userRepository;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource))
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout(logout -> logout.logoutUrl("/auth/logout")
-                        .addLogoutHandler((request, response, authentication) -> {
-                            final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-                            logout(authHeader);
-                        })
-                        .logoutSuccessHandler(
-                                (request, response, authentication) -> SecurityContextHolder.clearContext()));
-
-        return http.build();
-    }
-
-    private void logout(final String token) {
-        if (token == null || !token.startsWith("Bearer ")) {
-            throw new IllegalArgumentException("Invalid token");
-        }
-
-        final String jwtToken = token.substring(7);
-        final Token foundToken = tokenRepository.findByToken(jwtToken)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid token"));
-        foundToken.setExpired(true);
-        foundToken.setRevoked(true);
-        tokenRepository.save(foundToken);
-    }
+private final UserRepository userRepository;
 
     @Bean
     public UserDetailsService userDetailsService() {
